1741e6f215e5bf9a5acb09a53351061696569ac9
Khashan Amir <amirsmacbookpro@Khashans-MacBook-Pro.local>
2020-10-08 11:31:04 +0300

    [Amir]Another preperation for work after i copied the code that Dee did

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,13 +1,55 @@
-# def init_board():
-#     """Returns an empty 3-by-3 board (with .)."""
-#     board = []
-#     return board
+def init_board():
+    """Returns an empty 3-by-3 board (with .)."""
+    board = []
+    for row in range(3):
+        temp = []
+        for column in range(3):
+        
+            temp.append(".")
+        board.append(temp)
+
+    return board
+
+def get_input(text="Please enter row and column (ex: A2) "):
+    import sys
+
+    rows = ["A", "B", "C"]
+    cols = ["1", "2", "3"]
+
+    inp = []
+    user_text = input(text).upper()
+    if user_text == "QUIT" or user_text == "Q":
+        print("Thank you for playing! ")
+        sys.exit()
+
+    for char in user_text:
+        if char != " ":
+            inp.append(char)
+  
+    while inp[0] not in rows or inp[1] not in cols:
+        inp = []
+        user_text = input(text).upper()
+        for char in user_text:
+            if char != " ":
+                inp.append(char)
+
+    return (rows.index(inp[0]), cols.index(inp[1]))
 
 
-# def get_move(board, player):
-#     """Returns the coordinates of a valid move for player on board."""
-#     row, col = 0, 0
-#     return row, col
+def get_move(board, player):
+
+    position = get_input()
+    row = position[0]
+    col = position[1]
+
+    if board[row][col] == ".":
+        return (row, col)
+    else:
+        position = get_input("Please try again, position already occupied ")
+        # row = position[0]
+        # col = position[1]
+
+    return position
 
 
 # def get_ai_move(board, player):
@@ -16,14 +58,37 @@
 #     return row, col
 
 
-# def mark(board, player, row, col):
-#     """Marks the element at row & col on the board for player."""
-#     pass
+def mark(board, player, row, col):
+    if board[row][col] == ".":
+        board[row][col] = player
+
+    return board
 
 
-# def has_won(board, player):
-#     """Returns True if player has won the game."""
-#     return False
+def has_won(board, player):
+    combinations = [
+        [board[0][0], board[0][1], board[0][2]],
+        [board[1][0], board[1][1], board[1][2]],
+        [board[2][0], board[2][1], board[2][2]],
+        [board[0][0], board[1][0], board[2][0]],
+        [board[0][1], board[1][1], board[2][1]],
+        [board[0][2], board[1][2], board[2][2]],
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]],
+    ]
+
+    for combination in combinations:
+        occupied_by_player = 0
+
+        for cell in combination:
+            if cell == player:
+                occupied_by_player += 1
+
+        if occupied_by_player == 3:
+            return True
+
+    return False
+
 
 
 def is_full(board):
@@ -59,3 +124,5 @@ def print_board(board):
 
 # if __name__ == '__main__':
 #     main_menu()
+
+
