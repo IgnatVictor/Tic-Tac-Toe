c29d7bf36795097d089d5009cfd78218f74d96ce
Khashan Amir <amirsmacbookpro@Khashans-MacBook-Pro.local>
2020-10-08 10:39:01 +0300

    [Amir] I just commented the unused code

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,29 +1,29 @@
-def init_board():
-    """Returns an empty 3-by-3 board (with .)."""
-    board = []
-    return board
+# def init_board():
+#     """Returns an empty 3-by-3 board (with .)."""
+#     board = []
+#     return board
 
 
-def get_move(board, player):
-    """Returns the coordinates of a valid move for player on board."""
-    row, col = 0, 0
-    return row, col
+# def get_move(board, player):
+#     """Returns the coordinates of a valid move for player on board."""
+#     row, col = 0, 0
+#     return row, col
 
 
-def get_ai_move(board, player):
-    """Returns the coordinates of a valid move for player on board."""
-    row, col = 0, 0
-    return row, col
+# def get_ai_move(board, player):
+#     """Returns the coordinates of a valid move for player on board."""
+#     row, col = 0, 0
+#     return row, col
 
 
-def mark(board, player, row, col):
-    """Marks the element at row & col on the board for player."""
-    pass
+# def mark(board, player, row, col):
+#     """Marks the element at row & col on the board for player."""
+#     pass
 
 
-def has_won(board, player):
-    """Returns True if player has won the game."""
-    return False
+# def has_won(board, player):
+#     """Returns True if player has won the game."""
+#     return False
 
 
 def is_full(board):
@@ -36,26 +36,26 @@ def print_board(board):
     pass
 
 
-def print_result(winner):
-    """Congratulates winner or proclaims tie (if winner equals zero)."""
-    pass
+# def print_result(winner):
+#     """Congratulates winner or proclaims tie (if winner equals zero)."""
+#     pass
 
 
-def tictactoe_game(mode='HUMAN-HUMAN'):
-    board = init_board()
+# def tictactoe_game(mode='HUMAN-HUMAN'):
+#     board = init_board()
 
-    # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
-    print_board(board)
-    row, col = get_move(board, 1)
-    mark(board, 1, row, col)
+#     # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
+#     print_board(board)
+#     row, col = get_move(board, 1)
+#     mark(board, 1, row, col)
 
-    winner = 0
-    print_result(winner)
-
-
-def main_menu():
-    tictactoe_game('HUMAN-HUMAN')
+#     winner = 0
+#     print_result(winner)
 
 
-if __name__ == '__main__':
-    main_menu()
+# def main_menu():
+#     tictactoe_game('HUMAN-HUMAN')
+
+
+# if __name__ == '__main__':
+#     main_menu()
