284e410e6ea1fb19051202354e7003f391aaa70d
DianaGradinaru <diana.gradinaru.sincai@gmail.com>
2020-10-08 13:14:29 +0300

    Working version of has_won, and the player checks

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,19 +1,20 @@
+import sys
+
+
 def init_board():
     """Returns an empty 3-by-3 board (with .)."""
     board = []
     for row in range(3):
         temp = []
         for column in range(3):
-        
+
             temp.append(".")
         board.append(temp)
 
     return board
 
 
-
 def get_input(text="Please enter row and column (ex: A2) "):
-    import sys
 
     rows = ["A", "B", "C"]
     cols = ["1", "2", "3"]
@@ -27,7 +28,7 @@ def get_input(text="Please enter row and column (ex: A2) "):
     for char in user_text:
         if char != " ":
             inp.append(char)
-  
+
     while inp[0] not in rows or inp[1] not in cols:
         inp = []
         user_text = input(text).upper()
@@ -54,7 +55,6 @@ def get_move(board, player):
     return position
 
 
-
 # def get_ai_move(board, player):
 #     """Returns the coordinates of a valid move for player on board."""
 #     row, col = 0, 0
@@ -79,20 +79,15 @@ def has_won(board, player):
         [board[0][0], board[1][1], board[2][2]],
         [board[0][2], board[1][1], board[2][0]],
     ]
+    temp = []
 
-    for combination in combinations:
-        occupied_by_player = 0
+    for c in combinations:
+        t = []
+        for r in c:
+            t.append(r == player)
+        temp.append(all(t))
 
-        for cell in combination:
-            if cell == player:
-                occupied_by_player += 1
-
-        if occupied_by_player == 3:
-            return True
-
-    return False
-
-
+    return any(temp)
 
 
 def is_full(board):
@@ -106,53 +101,47 @@ def is_full(board):
 
 
 def print_board(board):
+    import os
+    func = "clear" if os.uname().sysname == "Linux" else "cls"
+    os.system(func)
 
-
-    print( f"""
+    print(
+        f"""
    1   2   3
 A  {board[0][0]} | {board[0][1]} | {board[0][2]}
   ---+---+---
 B  {board[1][0]} | {board[1][1]} | {board[1][2]}
   ---+---+---
 C  {board[2][0]} | {board[2][1]} | {board[2][2]}
-""")
-
-
-
+"""
+    )
 
 
 def print_result(board, winner):
-
     if has_won(board, winner):
         print(f"Congrats, {winner} wins!")
-
     else:
         print("It's a tie")
+    sys.exit()
 
 
-
-def tictactoe_game(mode='HUMAN-HUMAN'):
+def tictactoe_game(mode="HUMAN-HUMAN"):
     board = init_board()
-    player = 'X'
+    player = "X"
 
-    while not has_won(board, player) or not is_full(board):
-        # # use get_move(), mark(), has_won(), is_full(), and print_board() to create game logic
-
-        print(has_won(board, player))
-
+    while True:
         print_board(board)
-        
-        row, col = get_move(board, 1)
-        mark(board, player, row, col)
+        row, col = get_move(board, player)
+        board = mark(board, player, row, col)
 
-        if player == 'X':
-            player = '0'
+        if has_won(board, player) or is_full(board):
+            print_board(board)
+            print_result(board, player)
+
+        if player == "X":
+            player = "0"
         else:
-            player = 'X'
-
-        # winner = 0
-        # print_result(winner)
-    
+            player = "X"
 
 
 # # def main_menu():
@@ -174,4 +163,4 @@ def tictactoe_game(mode='HUMAN-HUMAN'):
 # print(is_full(test_board))
 
 
-tictactoe_game('HUMAN-HUMAN')
+tictactoe_game("HUMAN-HUMAN")
